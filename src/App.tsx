00e8adfe56833b570179f322c39522ac6c906cc4
zmeditation <senior.zsoftware@gmail.com>
2022-01-14 04:25:26 -0800

    layout -> Layout in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout";
 import { ThemeProvider, useTheme } from "@material-ui/core/styles";
 
 function App() {
