e86ceafa689ef686cafb2623eadc5d3ebd326dd4
zmeditation <senior.zsoftware@gmail.com>
2022-01-14 02:55:19 -0800

    add LandAccordion component, add AdminLand page.

--- src/pages/Admin/AdminLands/AdminLands.tsx
+++ src/pages/Admin/AdminLands/AdminLands.tsx
@@ -1,11 +1,19 @@
+import React from "react";
 import AdminTopTab from "../../../components/Admin/AdminTopTab/AdminTopTab";
+import LandAccordion from "./LandAccordion/LandAccordion";
 import { AdminLandsStyle } from "./AdminLandsStyle";
 export default function AdminLands() {
   const classes = AdminLandsStyle();
+  const [expanded, setExpanded] = React.useState<string | false>("panel1");
+
+  const handleChange =
+    (panel: string) => (event: React.ChangeEvent<{}>, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
   return (
     <>
       <AdminTopTab />
-      <div className={classes.root}>This is Admin Lands page!</div>
+      <LandAccordion />
     </>
   );
 }
