5883c3ede7c346605ce4e2ba89b7992cf01f5f29
zmeditation <senior.zsoftware@gmail.com>
2022-02-20 17:30:14 -0800

    completed bid contract frontend function in bid component.

--- src/pages/Bid/Bid.tsx
+++ src/pages/Bid/Bid.tsx
@@ -6,7 +6,6 @@ import CallMadeIcon from "@material-ui/icons/CallMade";
 
 import ActionButton from "../../components/Base/ActionButton";
 import TokenImg from "../../assets/img/1.png";
-import NeedSignIn from "..//NeedSignIn";
 import { useStyles, StyledInput } from "./BidStyle";
 import { BackButton } from "../../components/BackButton/BackButton";
 import settingicon from "../../assets/svg/bidpage_settingicon.svg";
@@ -60,11 +59,47 @@ const Bid = () => {
   const [uccAllowance, setUccAllowance] = useState(BigNumber.from(0));
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
+  signer = generateSigner(window.ethereum);
+
+  uccContract = generateContractInstance(
+    UccContractAddress,
+    UccContractAbi,
+    signer
+  );
+
+  bidContract = generateContractInstance(
+    BidContractAddress,
+    BidContractAbi,
+    signer
+  );
+
+  const initAllowance = async () => {
+    let allowance = await uccContract.allowance(
+      loginAddress,
+      BidContractAddress
+    );
+
+    if (!allowance.gt(0)) {
+      dispatch(
+        showAlert({
+          message:
+            "You have to first approve the marketplace contract to operate your asset. Please click Approve button.",
+          severity: "error",
+        })
+      );
+    }
+
+    setUccAllowance(allowance);
+  };
+
+  useEffect(() => {
+    initAllowance();
+    uccAllowance.gt(0) ? setBidStatus(false) : setBidStatus(true);
+  }, [uccAllowance]);
+
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
   };
-
-  var isSignIn = 1;
 
   const handleChange = (e: any) => {
     setPrice(e.target.value);
@@ -78,59 +113,30 @@ const Bid = () => {
   }, [selectedDate]);
 
   const handleApprove = async () => {
-    signer = generateSigner(window.ethereum);
-    uccContract = generateContractInstance(
-      UccContractAddress,
-      UccContractAbi,
-      signer
+    let approveMarketTx = await uccContract.approve(
+      BidContractAddress,
+      uccApprovalAmount
     );
+    await approveMarketTx.wait();
 
     let allowance = await uccContract.allowance(
       loginAddress,
       BidContractAddress
     );
-
     setUccAllowance(allowance);
-    if (!allowance.gt(0)) {
-      // approve marketplace contract to transfer this asset
-      dispatch(
-        showAlert({
-          message:
-            "You have to first approve the marketplace contract to operate your asset.",
-          severity: "error",
-        })
-      );
-      let approveMarketTx = await uccContract.approve(
-        BidContractAddress,
-        uccApprovalAmount
-      );
-      await approveMarketTx.wait();
 
-      allowance = await uccContract.allowance(loginAddress, BidContractAddress);
-      setUccAllowance(allowance);
-
-      dispatch(
-        showAlert({
-          message:
-            "Successfully approved. You have to confirm order creation transaction to finally publich your order.",
-          severity: "success",
-        })
-      );
-    }
+    dispatch(
+      showAlert({
+        message:
+          "Successfully approved. You have to confirm order creation transaction to finally publich your order.",
+        severity: "success",
+      })
+    );
   };
 
   const handleCancelApprove = async () => {};
 
   const handleBid = async () => {
-    if (timeStamp < minTime || timeStamp > maxTime) {
-      dispatch(
-        showAlert({
-          message: "Expiration date have to put between 1 minute and 1 year.",
-          severity: "error",
-        })
-      );
-      return;
-    }
     if (loginAddress.length === 0) {
       dispatch(
         showAlert({
@@ -139,6 +145,15 @@ const Bid = () => {
         })
       );
       navigate("/signin");
+      return;
+    }
+    if (timeStamp < minTime || timeStamp > maxTime) {
+      dispatch(
+        showAlert({
+          message: "Expiration date have to put between 1 minute and 1 year.",
+          severity: "error",
+        })
+      );
       return;
     }
 
@@ -151,13 +166,6 @@ const Bid = () => {
       );
       return;
     }
-
-    signer = generateSigner(window.ethereum);
-    bidContract = generateContractInstance(
-      BidContractAddress,
-      BidContractAbi,
-      signer
-    );
 
     let bidOrderTx = await bidContract[
       "placeBid(address,uint256,uint256,uint256)"
@@ -178,105 +186,93 @@ const Bid = () => {
     );
   };
 
-  useEffect(() => {
-    uccAllowance.gt(0) ? setBidStatus(false) : setBidStatus(true);
-  }, [uccAllowance]);
-
-  console.log("uccAllowance : ", uccAllowance);
-
   return (
     <div className={classes.root}>
-      {isSignIn === 1 ? (
-        <div className={classes.container_root}>
-          <BackButton className={classes.backButton} />
-          <div className={classes.bidCard}>
-            <div className={classes.leftCard}>
-              <div className={classes.imgContent}>
-                <img
-                  src={TokenImg}
-                  className={classes.tokenImg}
-                  alt='token'></img>
+      <div className={classes.container_root}>
+        <BackButton className={classes.backButton} />
+        <div className={classes.bidCard}>
+          <div className={classes.leftCard}>
+            <div className={classes.imgContent}>
+              <img
+                src={TokenImg}
+                className={classes.tokenImg}
+                alt='token'></img>
+            </div>
+          </div>
+          <div className={classes.rightCard}>
+            <div className={classes.title}>{t("Place a Bid")}</div>
+            <div className={classes.subtitle}>
+              {t("Set a price and expiration date for your bid on")}{" "}
+              <span>{t("Genesis Plaza")}</span>.
+            </div>
+            <div className={classes.form_field}>
+              <div className={classes.price_container}>
+                <Grid container>
+                  <Grid md={6} sm={6} xs={6} item>
+                    <div className={classes.subheader_label}>{t("PRICE")}</div>
+                    <FormControl>
+                      <StyledInput
+                        placeholder='0'
+                        onChange={(e) => handleChange(e)}
+                        startAdornment={
+                          <InputAdornment position='start'>
+                            <img src={settingicon} />
+                          </InputAdornment>
+                        }
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid md={6} sm={6} xs={6} item>
+                    <div className={classes.subheader_label}>
+                      {t("EXPIRATION DATE")}
+                    </div>
+                    <FormControl>
+                      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                          className={classes.datePicker}
+                          format='MM/dd/yyyy'
+                          margin='normal'
+                          id='date-picker-dialog'
+                          value={selectedDate}
+                          onChange={handleDateChange}
+                          KeyboardButtonProps={{
+                            "aria-label": "change date",
+                          }}
+                          keyboardIcon={<img src={calendar_icon} />}
+                        />
+                      </MuiPickersUtilsProvider>
+                    </FormControl>
+                  </Grid>
+                </Grid>
               </div>
             </div>
-            <div className={classes.rightCard}>
-              <div className={classes.title}>{t("Place a Bid")}</div>
-              <div className={classes.subtitle}>
-                {t("Set a price and expiration date for your bid on")}{" "}
-                <span>{t("Genesis Plaza")}</span>.
-              </div>
-              <div className={classes.form_field}>
-                <div className={classes.price_container}>
-                  <Grid container>
-                    <Grid md={6} sm={6} xs={6} item>
-                      <div className={classes.subheader_label}>
-                        {t("PRICE")}
-                      </div>
-                      <FormControl>
-                        <StyledInput
-                          placeholder='0'
-                          onChange={(e) => handleChange(e)}
-                          startAdornment={
-                            <InputAdornment position='start'>
-                              <img src={settingicon} />
-                            </InputAdornment>
-                          }
-                        />
-                      </FormControl>
-                    </Grid>
-                    <Grid md={6} sm={6} xs={6} item>
-                      <div className={classes.subheader_label}>
-                        {t("EXPIRATION DATE")}
-                      </div>
-                      <FormControl>
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                          <KeyboardDatePicker
-                            className={classes.datePicker}
-                            format='MM/dd/yyyy'
-                            margin='normal'
-                            id='date-picker-dialog'
-                            value={selectedDate}
-                            onChange={handleDateChange}
-                            KeyboardButtonProps={{
-                              "aria-label": "change date",
-                            }}
-                            keyboardIcon={<img src={calendar_icon} />}
-                          />
-                        </MuiPickersUtilsProvider>
-                      </FormControl>
-                    </Grid>
-                  </Grid>
-                </div>
-              </div>
-              {/* buttons */}
-              <div className={classes.buttons}>
-                <ActionButton
-                  color='light'
-                  className={classes.approve}
-                  onClick={bidStatus ? handleApprove : handleCancelApprove}>
-                  {bidStatus ? t("Approve") : t("Cancel Approve")}
-                  <CallMadeIcon fontSize='small' />
-                </ActionButton>
-                <ActionButton
-                  color='light'
-                  className={classes.bidchange}
-                  disabled={bidStatus}
-                  onClick={handleBid}>
-                  {t("Bid")}
-                  <CallMadeIcon fontSize='small' />
-                </ActionButton>
-                <ActionButton
-                  color='dark'
-                  className={classes.cancelchange}
-                  onClick={() => navigate(-1)}>
-                  {t("Cancel")}
-                </ActionButton>
-              </div>
+            {/* buttons */}
+            <div className={classes.buttons}>
+              <ActionButton
+                color='light'
+                className={classes.approve}
+                onClick={bidStatus ? handleApprove : handleCancelApprove}>
+                {bidStatus ? t("Approve") : t("Cancel Approve")}
+                <CallMadeIcon fontSize='small' />
+              </ActionButton>
+              <ActionButton
+                color='light'
+                className={classes.bidchange}
+                disabled={bidStatus}
+                onClick={handleBid}>
+                {t("Bid")}
+                <CallMadeIcon fontSize='small' />
+              </ActionButton>
+              <ActionButton
+                color='dark'
+                className={classes.cancelchange}
+                onClick={() => navigate(-1)}>
+                {t("Cancel")}
+              </ActionButton>
             </div>
           </div>
         </div>
-      ) : (
-        <NeedSignIn />
-      )}
+      </div>
     </div>
   );
 };
