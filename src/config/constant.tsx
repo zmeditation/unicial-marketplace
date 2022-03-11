68a84ed4df31d2086d7c5ae997104c91a5e1c4a4
zmeditation <senior.zsoftware@gmail.com>
2022-03-11 19:22:27 +0900

    modify all map component

--- src/config/constant.tsx
+++ src/config/constant.tsx
@@ -1,5 +1,5 @@
-export const ApiUrl = "https://api.unicial.org";
-// export const ApiUrl = "http://localhost:3000";
+// export const ApiUrl = "https://api.unicial.org";
+export const ApiUrl = "http://localhost:8080";
 export const REQUEST_API_URL = `${ApiUrl}/api/v1`;
 
 export const tabs = {
