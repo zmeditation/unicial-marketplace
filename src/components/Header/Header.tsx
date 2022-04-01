dc5b084e90f7f37dcc7802b8475e7478772d9618
zmeditation <senior.zsoftware@gmail.com>
2022-04-01 19:25:25 +0300

    modified Header.tsx, HeaderMobileMenu.tsx for Builder link.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import { HeaderStyle } from "./HeaderStyle";
 import HeaderMobileMenu from "./component/HeaderMobileMenu/HeaderMobileMenu";
@@ -7,52 +7,85 @@ import HeaderSignInBtn from "./component/HeaderSignInBtn/HeaderSignInBtn";
 import { useAppSelector } from "../../store/hooks";
 import { selectLoginAddress } from "../../store/auth/selectors";
 import { useTranslation } from "react-i18next";
+import { useState, useEffect } from "react";
+import { headerLinkData } from "../../config/constant";
 
 export default function Header() {
   const classes = HeaderStyle();
   const navigate = useNavigate();
+  const location = useLocation();
   const loginAddress = useAppSelector(selectLoginAddress);
   const { t } = useTranslation();
 
+  const [headerActive, setHeaderActive] = useState(headerLinkData.marketplace);
   const handleSignIn = () => {
     navigate(`/signin`);
   };
 
-  const handleRoute = (route: string) => {
-    window.open(route);
+  const handleHeaderlink = (index: number) => {
+    setHeaderActive(index);
+    switch (index) {
+      case headerLinkData.marketplace:
+        navigate("/");
+        break;
+      case headerLinkData.builder:
+        navigate("/builder");
+        break;
+      case headerLinkData.docs:
+        window.open("https://doc.unicial.org");
+        break;
+      case headerLinkData.blog:
+        window.open("https://blog.unicial.org");
+        break;
+    }
   };
+  useEffect(() => {
+    if (location.pathname.includes("/builder")) {
+      setHeaderActive(headerLinkData.builder);
+    } else {
+      setHeaderActive(headerLinkData.marketplace);
+    }
+  }, [headerActive]);
 
   return (
     <>
       <div className={classes.root}>
         <div className={classes.container}>
           <div className={classes.headermenuContainer}>
-            <Link to="/" className={classes.logoContent}>
+            <a href="https://unicial.org" className={classes.logoContent}>
               <img src={"/logo.svg"} className={classes.logo} alt="symbol" />
               <span className={classes.logoName}>{t("UNICIAL")}</span>
-            </Link>
+            </a>
             <Button
-              className={classes.headerBtn}
+              className={
+                headerActive === headerLinkData.marketplace
+                  ? classes.headerClickBtn
+                  : classes.headerBtn
+              }
               disableRipple
-              onClick={() => handleRoute("https://unicial.org")}
-            >
-              <span></span>
-              <span className={classes.headerLink}>{t("Home")}</span>
-              <span className={"active-border"}></span>
-            </Button>
-            <Button
-              className={classes.headerClickBtn}
-              disableRipple
-              onClick={() => navigate("/")}
+              onClick={() => handleHeaderlink(headerLinkData.marketplace)}
             >
               <span></span>
               <span className={classes.headerLink}>{t("Marketplace")}</span>
               <span className={"active-border"}></span>
             </Button>
             <Button
+              className={
+                headerActive === headerLinkData.builder
+                  ? classes.headerClickBtn
+                  : classes.headerBtn
+              }
+              disableRipple
+              onClick={() => handleHeaderlink(headerLinkData.builder)}
+            >
+              <span></span>
+              <span className={classes.headerLink}>{t("Builder")}</span>
+              <span className={"active-border"}></span>
+            </Button>
+            <Button
               className={classes.headerBtn}
               disableRipple
-              onClick={() => handleRoute("https://doc.unicial.org")}
+              onClick={() => handleHeaderlink(headerLinkData.docs)}
             >
               <span></span>
               <span className={classes.headerLink}>{t("Documents")}</span>
@@ -61,7 +94,7 @@ export default function Header() {
             <Button
               className={classes.headerBtn}
               disableRipple
-              onClick={() => handleRoute("https://blog.unicial.org")}
+              onClick={() => handleHeaderlink(headerLinkData.blog)}
             >
               <span></span>
               <span className={classes.headerLink}>{t("Blog")}</span>
