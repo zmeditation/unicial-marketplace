820133d559e3f11d74694092212b404c20422949
zmeditation <senior.zsoftware@gmail.com>
2022-04-13 09:25:55 -0700

    change round back button component icon picture

--- src/components/Base/RoundBackBtn.tsx
+++ src/components/Base/RoundBackBtn.tsx
@@ -1,17 +1,23 @@
+/** @format */
+
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import leftArrow from "../../assets/svg/leftarrow_icon.svg";
+import multipylIcon from "../../assets/svg/multiply_icon.svg";
 import clsx from "clsx";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       cursor: "pointer",
-      borderRadius: "100px",
       minWidth: "35px",
       width: "35px",
       height: "35px",
       justifyContent: "center",
       display: "flex",
       alignItems: "center",
-      background: "#444858",
+      background: "rgba(255, 255, 255, 0.1)",
+      opacity: 0.9,
+      border: "1px solid rgba(255, 255, 255, 0.18)",
+      borderRadius: "66px",
       "& i": {
         fontSize: "22px",
         fontWeight: 100,
@@ -32,9 +38,9 @@ export default function RoundBackBtn({ className, onBack, type }: Props) {
   return (
     <div className={clsx(classes.root, className)} onClick={onBack}>
       {type === "multiply" ? (
-        <i className="fas fa-times"></i>
+        <img src={multipylIcon} alt='multiplyicon' />
       ) : (
-        <i className="fas fa-angle-left"></i>
+        <img src={leftArrow} alt='leftarrowicon' />
       )}
     </div>
   );
